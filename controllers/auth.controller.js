863861a393e94ae8264e4045d32735a8a54a577f
pushkar nawani <pushkar.nawani.contractor@hilton.com>
2020-07-28 00:27:16 +0530

    files up

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -50,14 +50,14 @@ const signIn = async (req, res) => {
     });
     if (!user)
       return res.status(400).json({
-        data: [{ feild: "invalid-user", message: "User Not Exist" }],
+        data: [{ feild: "email", message: "User Not Exist" }],
         message: "User Not Exist",
       });
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
       return res.status(400).json({
-        data: [{ feild: "username", message: "Incorrect Password" }],
+        data: [{ feild: "password ", message: "Incorrect Password" }],
         message: "Incorrect Password !",
       });
 
